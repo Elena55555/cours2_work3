5eb71eec2cd232225af2a0112360a9b3385717d2
Elena <elenela@yandex.ru>
2023-06-07 23:06:36 +0300

    Домашняя работа № 3

---
+++ src/Car.java
@@ -0,0 +1,12 @@
+public class Car extends Transport implements Checkable  {
+    public Car(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+
+    @Override
+    public void check() {
+        this.updateTyre();
+        this.checkEngine();
+
+    }
+}
